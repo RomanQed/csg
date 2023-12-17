f33306bf53dab9e7d4e53eeeb2dde64d2b98258d
RomanQed <gbakaldin@gmail.com>
2023-12-17 21:04:28 +0300

    Add comments

--- src/main/java/com/github/romanqed/graphic/FrustumClipper.java
+++ src/main/java/com/github/romanqed/graphic/FrustumClipper.java
@@ -26,13 +26,19 @@ public final class FrustumClipper implements Clipper {
 
     private boolean isOutside(Vector3d v1, Vector3d v2, Vector3d v3) {
         return
+                // near
                 (frustum.near.compare(v1) == -1 && frustum.near.compare(v2) == -1 && frustum.near.compare(v3) == -1)
+                // far
                 || (frustum.far.compare(v1) == -1 && frustum.far.compare(v2) == -1 && frustum.far.compare(v3) == -1)
+                // left
                 || (frustum.left.compare(v1) == -1 && frustum.left.compare(v2) == -1 && frustum.left.compare(v3) == -1)
+                // right
                 || (frustum.right.compare(v1) == -1
                         && frustum.right.compare(v2) == -1
                         && frustum.right.compare(v3) == -1)
+                // top
                 || (frustum.top.compare(v1) == -1 && frustum.top.compare(v2) == -1 && frustum.top.compare(v3) == -1)
+                // bottom
                 || (frustum.bottom.compare(v1) == -1
                         && frustum.bottom.compare(v2) == -1
                         && frustum.bottom.compare(v3) == -1);
