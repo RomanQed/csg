ae1e76ce69b586d84da2cfbd2b6d2fa908591f01
RomanQed <gbakaldin@gmail.com>
2023-12-17 21:03:44 +0300

    Replace if ops with ||

--- src/main/java/com/github/romanqed/graphic/FrustumClipper.java
+++ src/main/java/com/github/romanqed/graphic/FrustumClipper.java
@@ -25,27 +25,17 @@ public final class FrustumClipper implements Clipper {
     }
 
     private boolean isOutside(Vector3d v1, Vector3d v2, Vector3d v3) {
-        // near, far
-        if (frustum.near.compare(v1) == -1 && frustum.near.compare(v2) == -1 && frustum.near.compare(v3) == -1) {
-            return true;
-        }
-        if (frustum.far.compare(v1) == -1 && frustum.far.compare(v2) == -1 && frustum.far.compare(v3) == -1) {
-            return true;
-        }
-        // left, right
-        if (frustum.left.compare(v1) == -1 && frustum.left.compare(v2) == -1 && frustum.left.compare(v3) == -1) {
-            return true;
-        }
-        if (frustum.right.compare(v1) == -1 && frustum.right.compare(v2) == -1 && frustum.right.compare(v3) == -1) {
-            return true;
-        }
-        // top, bottom
-        if (frustum.top.compare(v1) == -1 && frustum.top.compare(v2) == -1 && frustum.top.compare(v3) == -1) {
-            return true;
-        }
-        return frustum.bottom.compare(v1) == -1
-                && frustum.bottom.compare(v2) == -1
-                && frustum.bottom.compare(v3) == -1;
+        return
+                (frustum.near.compare(v1) == -1 && frustum.near.compare(v2) == -1 && frustum.near.compare(v3) == -1)
+                || (frustum.far.compare(v1) == -1 && frustum.far.compare(v2) == -1 && frustum.far.compare(v3) == -1)
+                || (frustum.left.compare(v1) == -1 && frustum.left.compare(v2) == -1 && frustum.left.compare(v3) == -1)
+                || (frustum.right.compare(v1) == -1
+                        && frustum.right.compare(v2) == -1
+                        && frustum.right.compare(v3) == -1)
+                || (frustum.top.compare(v1) == -1 && frustum.top.compare(v2) == -1 && frustum.top.compare(v3) == -1)
+                || (frustum.bottom.compare(v1) == -1
+                        && frustum.bottom.compare(v2) == -1
+                        && frustum.bottom.compare(v3) == -1);
     }
 
     @Override
